e313f63e038d01724c70364b1e7175f58954f066
Muhammad Zeeshan <m.zeeshan6789539@gmail.com>
2026-05-16 08:14:04 +0500

    add husky

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -21,6 +21,20 @@ export default function Home() {
           العربية
         </Link>
       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         {t("title")}
       </main>
